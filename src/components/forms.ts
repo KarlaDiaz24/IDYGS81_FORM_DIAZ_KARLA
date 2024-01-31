c932bacdca451232f703eafc1f78953f4b312058
Karla Diaz <montsedays24@gmail.com>
2024-01-30 20:45:34 -0500

    m

--- src/components/forms.ts
+++ src/components/forms.ts
@@ -1,24 +1,17 @@
-// Archivo en el directorio src/utils/usuarios.ts
-
-// Definir el tipo para el usuario
 interface Usuario {
   nombre: string;
   apellido: string;
   edad: number,
   genero: string,
   telefono: number
-  // Agrega otras propiedades según sea necesario
 }
 
-// Lista de usuarios
 const usuarios: Usuario[] = [];
 
-// Función para verificar si el nombre ya existe en la lista de usuarios
 export function verificarNombreExistente(nombre: string): boolean {
   return usuarios.some((user) => user.nombre === nombre);
 }
 
-// Función para agregar un nuevo usuario a la lista
 export function agregarUsuario(usuario: Usuario): void {
   usuarios.push(usuario);
 }
